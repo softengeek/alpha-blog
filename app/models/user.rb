d5eb6030f7878f84a6b0b739e848faa90ecf03cd
Rails Udemy Course <softengeekkhadz@gmail.com>
2017-10-31 01:16:45 +0000

    validated users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  
+  validates :username, presence: true, uniqueness:  { case_sesnsitive: false }, length: { minimum: 10, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniuemess: { case_sesnsitive: false }, format: {with: VALID_EMAIL_REGEX }, length: { minimum: 10, maximum: 60 }
 end
